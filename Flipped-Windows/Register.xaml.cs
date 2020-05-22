d3d80f411998e0b95017f418e00430c541ba6050
Richard <38907693+Mr-Second@users.noreply.github.com>
2020-05-22 23:36:32 +0800

    --stroy=1003379 --user=郑康

---
+++ Flipped-Windows/Register.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Flipped_Win10
+{
+    /// <summary>
+    /// Register.xaml 的交互逻辑
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
