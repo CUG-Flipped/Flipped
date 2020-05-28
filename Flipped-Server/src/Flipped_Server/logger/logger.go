18a8ea39abbc205d14040af678ab3409d33c3d9c
Richard <38907693+Mr-Second@users.noreply.github.com>
2020-05-28 23:34:45 +0800

    --story=1004011,1004239 --user=郑康

---
+++ Flipped-Server/src/Flipped_Server/logger/logger.go
@@ -0,0 +1,63 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+var(
+	Logger = logrus.New()
+)
+
+func InitLog() {
+	// 设置日志格式为json格式
+	Logger.Formatter = &logrus.JSONFormatter{}
+	file, err := os.OpenFile("./log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err == nil {
+		Logger.SetOutput(file)
+		Logger.SetLevel(logrus.InfoLevel)
+	} else {
+		panic(err)
+	}
+}
+
+func SetToLogger(level logrus.Level, function string, msg string, otherMsg string){
+	switch level {
+		case logrus.DebugLevel:{
+			Logger.WithFields(logrus.Fields{
+				"function": function,
+				"msg": msg,
+			}).Debug(otherMsg)
+		}
+		case logrus.InfoLevel:{
+			Logger.WithFields(logrus.Fields{
+				"function": function,
+				"msg": msg,
+			}).Info(otherMsg)
+		}
+		case logrus.WarnLevel:{
+			Logger.WithFields(logrus.Fields{
+				"function": function,
+				"msg": msg,
+			}).Warning(otherMsg)
+		}
+		case logrus.ErrorLevel:{
+			Logger.WithFields(logrus.Fields{
+				"function": function,
+				"msg": msg,
+			}).Error(otherMsg)
+		}
+		case logrus.FatalLevel:{
+			Logger.WithFields(logrus.Fields{
+				"function": function,
+				"msg": msg,
+			}).Fatal(otherMsg)
+		}
+		default: {
+			Logger.WithFields(logrus.Fields{
+				"function": function,
+				"msg": msg,
+			}).Debug(otherMsg)
+		}
+	}
+}
